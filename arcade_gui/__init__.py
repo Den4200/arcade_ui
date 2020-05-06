12e29eac0662ebcba715ac7307358bac3caf6f35
Den4200 <dpham.42@hotmail.com>
2020-05-06 19:49:05 -0400

    add ImageButton to __all__

--- arcade_gui/__init__.py
+++ arcade_gui/__init__.py
@@ -1,4 +1,5 @@
+from arcade_gui.image_button import ImageButton
 from arcade_gui.text_input import TextInput
 
-__all__ = ('TextInput',)
+__all__ = ('ImageButton', 'TextInput',)
 __version__ = '0.0.1'
