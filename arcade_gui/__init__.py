c6654b00bb38c496fef7ee16ef0af85ae9010f3c
Den4200 <dpham.42@hotmail.com>
2020-05-06 19:51:18 -0400

    remove trailing comma from __all__

--- arcade_gui/__init__.py
+++ arcade_gui/__init__.py
@@ -1,5 +1,5 @@
 from arcade_gui.image_button import ImageButton
 from arcade_gui.text_input import TextInput
 
-__all__ = ('ImageButton', 'TextInput',)
+__all__ = ('ImageButton', 'TextInput')
 __version__ = '0.0.1'
