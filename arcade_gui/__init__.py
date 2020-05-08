8062b9f445a026ba36be8b618eb58320c19d4121
Den4200 <dpham.42@hotmail.com>
2020-05-07 22:04:44 -0400

    move buttons to buttons.py and create TextButton

--- arcade_gui/__init__.py
+++ arcade_gui/__init__.py
@@ -1,6 +1,6 @@
-from arcade_gui.image_button import ImageButton
+from arcade_gui.buttons import ImageButton, TextButton
 from arcade_gui.text_box import TextBox
 from arcade_gui.text_input import TextInput
 
-__all__ = ('ImageButton', 'TextBox', 'TextInput')
+__all__ = ('ImageButton', 'TextBox', 'TextButton', 'TextInput')
 __version__ = '0.0.1'
---
+++ arcade_gui/buttons.py
@@ -0,0 +1,76 @@
+from typing import Any, List
+
+import arcade
+
+from arcade_gui.text_box import TextBox
+from arcade_gui.utils import check_point_for_collision
+
+
+class ButtonBehavior:
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int) -> bool:
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            if check_point_for_collision(
+                (x, y),
+                self.center_x - self.viewport[0] - self.width / 2,
+                self.center_x - self.viewport[0] + self.width / 2,
+                self.center_y - self.viewport[1] - self.width / 2,
+                self.center_y - self.viewport[1] + self.width / 2
+            ):
+                self.pressed = True
+                return True
+
+            return False
+
+        return False
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int) -> bool:
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            if check_point_for_collision(
+                (x, y),
+                self.center_x - self.viewport[0] - self.width / 2,
+                self.center_x - self.viewport[0] + self.width / 2,
+                self.center_y - self.viewport[1] - self.width / 2,
+                self.center_y - self.viewport[1] + self.width / 2
+            ):
+                self.on_submit()
+                self.pressed = False
+                return True
+
+            return False
+
+        return False
+
+    def on_submit(self) -> None:
+        pass
+
+
+class ImageButton(ButtonBehavior, arcade.Sprite):
+
+    def __init__(
+        self,
+        clicked: str,
+        normal: str,
+        viewport: List[float],  # Must be a mutable object!
+        **kwargs: Any
+    ) -> None:
+        super().__init__(filename=normal, **kwargs)
+
+        self.clicked = arcade.load_texture(clicked)
+        self.normal = arcade.load_texture(normal)
+
+        self.viewport = viewport
+
+        self.pressed = False
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int) -> None:
+        if super().on_mouse_press(x, y, button, modifiers):
+            self.texture = self.clicked
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int) -> None:
+        if super().on_mouse_release(x, y, button, modifiers):
+            self.texture = self.normal
+
+
+class TextButton(TextBox, ButtonBehavior):
+    pass
