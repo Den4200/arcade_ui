c9f8a417e23ae2c85f5a9d0d29ac4433ac8f23f1
Den4200 <dpham.42@hotmail.com>
2020-05-13 20:26:30 -0400

    add Panel to `__all__`

--- arcade_gui/__init__.py
+++ arcade_gui/__init__.py
@@ -1,9 +1,10 @@
 from arcade_gui.buttons import ImageButton, TextButton
 from arcade_gui.event_map import EventMap
+from arcade_gui.panel import Panel
 from arcade_gui.text_box import TextBox
 from arcade_gui.text_input import TextInput
 from arcade_gui.widgets import InteractiveWidget
 from arcade_gui.view import View
 
-__all__ = ('EventMap', 'InteractiveWidget', 'ImageButton', 'TextBox', 'TextButton', 'TextInput', 'View')
+__all__ = ('EventMap', 'InteractiveWidget', 'ImageButton', 'Panel', 'TextBox', 'TextButton', 'TextInput', 'View')
 __version__ = '0.0.1'
