aaf9484821ff1dcb4928fb991c6601c0e14df89e
Den4200 <dpham.42@hotmail.com>
2020-05-18 22:07:54 -0400

    reorganize arcade_gui/__init__.py

--- arcade_gui/__init__.py
+++ arcade_gui/__init__.py
@@ -6,5 +6,16 @@ from arcade_gui.text_input import TextInput
 from arcade_gui.widgets import InteractiveWidget
 from arcade_gui.view import View
 
-__all__ = ('EventMap', 'InteractiveWidget', 'ImageButton', 'Panel', 'TextBox', 'TextButton', 'TextInput', 'View')
+
 __version__ = '0.0.1'
+
+__all__ = (
+    'EventMap',
+    'InteractiveWidget',
+    'ImageButton',
+    'Panel',
+    'TextBox',
+    'TextButton',
+    'TextInput',
+    'View'
+)
